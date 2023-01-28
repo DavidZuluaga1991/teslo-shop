80b656d22ef635ec97d1c9bf564a1ecc65bee4d0
David Zuluaga <davidzuluaga1991@gmail.com>
2023-01-27 23:57:27 -0500

    learning to use typeorm

---
+++ README.md
@@ -0,0 +1,16 @@
+<p align="center">
+  <a href="http://nestjs.com/" target="blank"><img src="https://nestjs.com/img/logo-small.svg" width="200" alt="Nest Logo" /></a>
+</p>
+
+# TesloDB
+
+1. Clonar proyecto
+2. ``` npm i ```
+3. Clonar el archivo ```.env.template``` y renombrarlo a ```.env```
+4. Camvbiar las variables de entorno
+5. Levantar la base de datos
+
+```
+docker-compose -d
+```
+6. Levanta: ```npm run start:dev```
---
+++ src/products/products.service.ts
@@ -0,0 +1,48 @@
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { HttpException } from '@nestjs/common/exceptions';
+import { ErrorHandler } from '@nestjs/common/interfaces';
+import { ErrorHttpStatusCode } from '@nestjs/common/utils/http-error-by-code.util';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+import { Product } from './entities/product.entity';
+
+@Injectable()
+export class ProductsService {
+  constructor(
+    @InjectRepository(Product)
+    private readonly productRepository: Repository<Product>,
+  ) {}
+
+  async create(createProductDto: CreateProductDto) {
+    try {
+      const product = this.productRepository.create(createProductDto);
+      await this.productRepository.save(product);
+      return product;
+    } catch (error) {
+      this.handleExceptions(error);
+      // throw new InternalServerErrorException('Ayuda!!');
+    }
+  }
+
+  findAll() {
+    return `This action returns all products`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} product`;
+  }
+
+  update(id: number, updateProductDto: UpdateProductDto) {
+    return `This action updates a #${id} product`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} product`;
+  }
+
+  private handleExceptions(error: HttpException) {
+    console.log(error);
+  }
+}
